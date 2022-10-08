2e2ed15fdbecdcc2f97d4d4a3f870bf52b9bf734
manishchandra-resiliencesoft <manishchandra@resiliencesoft.com>
2022-10-08 18:21:54 +0530

    connectedto db

--- src/employee/employee.resolver.ts
+++ src/employee/employee.resolver.ts
@@ -1,8 +1,12 @@
 import { Resolver, Query } from '@nestjs/graphql';
+import { EmployeeService } from './employee.service';
 import { Employee } from './entities/employee.entity';
 
 @Resolver(() => Employee)
 export class EmployeeResolver {
-  @Query(() => Employee)
-  findAll() {}
+  constructor(private employeeService: EmployeeService) {}
+  @Query(() => Employee, { name: 'findAllEmployee' })
+  findAll() {
+    this.employeeService.findAll();
+  }
 }
